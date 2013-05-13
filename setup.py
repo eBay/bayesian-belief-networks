5408485d5020f9022601c48cab7ebc80a42dd409
Neville Newey <nn@milo.com>
2013-05-13 14:12:32 -0700

    Bumped version to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Bayesian',
-    version='0.1.3',
+    version='0.1.4',
     author='Neville Newey',
     author_email='nn@theneweys.org',
     packages=['bayesian', 'bayesian.test', 'bayesian.examples'],
