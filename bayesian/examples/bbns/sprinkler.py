c1969c1332cf5b446644b7618f21f9ac9b297cd8
Neville Newey <nnewey@ebay.com>
2013-09-13 17:04:34 -0700

    Fixed Sprinkler example and moved it to bbns directory.

---
+++ bayesian/examples/bbns/sprinkler.py
@@ -0,0 +1,40 @@
+'''Example from Wikipedia: http://en.wikipedia.org/wiki/Bayesian_network'''
+
+from bayesian.bbn import *
+from bayesian.utils import make_key
+
+def f_rain(rain):
+    if rain.value is True:
+        return 0.2
+    return 0.8
+
+
+def f_sprinkler(rain, sprinkler):
+    if rain.value is False and sprinkler.value is True:
+        return 0.4
+    if rain.value is False and sprinkler.value is False:
+        return 0.6
+    if rain.value is True and sprinkler.value is True:
+        return 0.01
+    if rain.value is True and sprinkler.value is False:
+        return 0.99
+
+
+def f_grass_wet(sprinkler, rain, grass_wet):
+    table = dict()
+    table['fft'] = 0.0
+    table['fff'] = 1.0
+    table['ftt'] = 0.8
+    table['ftf'] = 0.2
+    table['tft'] = 0.9
+    table['tff'] = 0.1
+    table['ttt'] = 0.99
+    table['ttf'] = 0.01
+    return table[make_key(sprinkler, rain, grass_wet)]
+
+
+if __name__ == '__main__':
+    g = build_bbn(
+        f_rain,
+        f_sprinkler,
+        f_grass_wet)
